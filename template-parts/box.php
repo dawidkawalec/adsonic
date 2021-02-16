547deb7ce69066cb79bf3c668ceb5b5fb40f0f25
Dawid Kawa <kontakt@dawidkawalec.pl>
2021-02-16 19:39:30 +0100

    poprawki rwd

--- template-parts/box.php
+++ template-parts/box.php
@@ -15,26 +15,32 @@
         </div>
         <div class="row justify-content-lg-between">
             <div class="col-lg-5 box-single">
-                <div class="icon"><img src="/wp-content/themes/adsonic/inc/assets/image/icon.png" alt="Adsonic logo"></div>
+                <div class="icon"><img src="/wp-content/uploads/2021/02/ikon1.svg" alt="Adsonic logo"></div>
                 <div class="desc">
-                Tworzymy kompleksowe strategie kampanii tak, aby trafić z odpowiednią reklamą do klienta na każdym z etapów ścieżki zakupowej - <span>koniec z przepalaniem budżetu na nietrafione kreacje!</span>
+                    Tworzymy kompleksowe strategie kampanii tak, aby trafić z odpowiednią reklamą do klienta na każdym z
+                    etapów ścieżki zakupowej - <span>koniec z przepalaniem budżetu na nietrafione kreacje!</span>
                 </div>
             </div>
             <div class="col-lg-5 box-single text-lg-right">
-            <div class="icon"><img src="/wp-content/themes/adsonic/inc/assets/image/icon.png" alt="Adsonic logo"></div>
+                <div class="icon"><img src="/wp-content/uploads/2021/02/ikon2.svg" alt="Adsonic logo"></div>
                 <div class="desc">
-                Doświadczenie w dostarczaniu wysokiej jakości usług dla branży <span>e-commerce</span> pozwala nam oferować rozwiązania, które gwarantują wyższą stopę zwrotu kosztów przeznaczonych na reklamę - doświadczenia ma znaczenie!                </div>
+                    Doświadczenie w dostarczaniu wysokiej jakości usług dla branży <span>e-commerce</span> pozwala nam
+                    oferować rozwiązania, które gwarantują wyższą stopę zwrotu kosztów przeznaczonych na reklamę -
+                    doświadczenia ma znaczenie! </div>
             </div>
             <div class="col-lg-5 box-single">
-            <div class="icon"><img src="/wp-content/themes/adsonic/inc/assets/image/icon.png" alt="Adsonic logo"></div>
+                <div class="icon"><img src="/wp-content/uploads/2021/02/ikon3.svg" alt="Adsonic logo"></div>
                 <div class="desc">
-                <span>Poprawimy UX Twojego sklepu</span>, aby kompleksowo zadbać o użyteczność oraz obraz Twojego serwisu w oczach klienta - bo pierwsze wrażenie ma znaczenie!
+                    <span>Poprawimy UX Twojego sklepu</span>, aby kompleksowo zadbać o użyteczność oraz obraz Twojego
+                    serwisu w oczach klienta - bo pierwsze wrażenie ma znaczenie!
                 </div>
             </div>
             <div class="col-lg-5 box-single  text-lg-right">
-            <div class="icon"><img src="/wp-content/themes/adsonic/inc/assets/image/icon.png" alt="Adsonic logo"></div>
+                <div class="icon"><img src="/wp-content/uploads/2021/02/ikon4.svg" alt="Adsonic logo"></div>
                 <div class="desc">
-                <span>Wykonamy dla Ciebie darmowy audyt konta</span>, dzięki czemu dowiesz się co poprawić lub odświeżyć, aby zwiększyć efektywność reklam - sprawdź sam, o ile więcej klientów możesz zdobywać dzięki skutecznym kampaniom online.
+                    <span>Wykonamy dla Ciebie darmowy audyt konta</span>, dzięki czemu dowiesz się co poprawić lub
+                    odświeżyć, aby zwiększyć efektywność reklam - sprawdź sam, o ile więcej klientów możesz zdobywać
+                    dzięki skutecznym kampaniom online.
                 </div>
             </div>
         </div>
